e4f2a8006729a04567d2cba7a272446d640a5828
ayushch0001 <ayushch0001@gmail.com>
2023-07-31 18:55:10 +0530

    first project commit

---
+++ src/com/gitrepo/NumberOperation.java
@@ -0,0 +1,14 @@
+package com.gitrepo;
+
+public class NumberOperation {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("a+b "+ 5+6);
+		System.out.println("a-b "+ (7-6));
+		System.out.println("a/b "+ 6/3);
+
+		
+	}
+
+}
